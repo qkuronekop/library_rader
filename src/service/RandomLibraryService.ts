795d8608152bc52901775202b887bb10a2c96c7d
qkuronekop <xsarasinax@gmail.com>
2023-01-28 13:57:04 +0900

    pathの変更

--- src/service/RandomLibraryService.ts
+++ src/service/RandomLibraryService.ts
@@ -3,7 +3,7 @@ import { Prefectures } from '../models/Prefectures';
 import { Library } from "../models/Library";
 
 class RandomLibraryService {
-  private URL = "/api/library?appkey=a7606ce7ca5e7be0119f04b9092a72d9&pref={pref}&format=json&callback=";
+  private URL = "https://calil.jp/library?appkey=a7606ce7ca5e7be0119f04b9092a72d9&pref={pref}&format=json&callback=";
 
   public async getRandomLibrary():Promise<Library> {
     const randomIndex = Math.floor(Math.random() * 47);
